2a7600204e78dab212afd9e632815ea7a93bb76f
No7777 <552340364@qq.com>
2016-04-07 16:40:33 +0800

    0000

--- zhihu/settings.py
+++ zhihu/settings.py
@@ -24,7 +24,7 @@ USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Fi
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
+DOWNLOAD_DELAY=3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
@@ -61,9 +61,11 @@ USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Fi
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'zhihu.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'zhihu.pipelines.SomePipeline': 300,
+}
+MONGO_URL = "127.0.0.1:27017"
+MONGO_DATABASE = "qiubai"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
